42d7df7ae61f3034d4112efa11642b90b0dbb035
AndreNIH <31828626+AndreNIH@users.noreply.github.com>
2023-09-10 11:16:30 -0700

    wip: sequelize sync errors

--- src/models/index.js
+++ src/models/index.js
@@ -7,15 +7,47 @@ const FinancialTransaction = require('./financial_transaction')
 const Funding = require('./funding')
 const Distributor = require('./distributor')
 
+//Set up associations
+
+//User->Patient 1:N
+User.hasMany(Psychologist)
+Psychologist.belongsTo(User)
+
+//User->Psychologist 1:1
+User.hasOne(Psychologist)
+Psychologist.belongsTo(User)
+
+
+//Setup link beween FinancialTransaction and User, 
+//TODO:
+
+FinancialTransaction.hasMany(Funding)
+Funding.belongsTo(FinancialTransaction)
+
+FinancialTransaction.hasMany(Distributor)
+Distributor.belongsTo(FinancialTransaction)
+
+
+User.hasMany(FinancialTransaction, {as: 'idReceiver'})
+FinancialTransaction.belongsTo(User)
+
+Volunteer.hasMany(FinancialTransaction)
+FinancialTransaction.belongsTo(Volunteer)
+//------------------
+SupportMessage.hasMany(Funding)
+Funding.belongsTo(SupportMessage)
+
+const forceUpdate = true;
 //Create tables matching model schema
-User.sync()
-Psychologist.sync()
-Patient.sync()
-Volunteer.sync()
-SupportMessage.sync()
-FinancialTransaction.sync()
-Funding.sync()
-Distributor.sync()
+
+Volunteer.sync({alter:true, force:forceUpdate})
+Funding.sync({alter:true, force:forceUpdate})
+Distributor.sync({alter:true, force:forceUpdate})
+FinancialTransaction.sync({alter:true, force:forceUpdate})
+User.sync({alter:true, force:forceUpdate})
+Psychologist.sync({alter:true, force:forceUpdate})
+SupportMessage.sync({alter:true, force:forceUpdate})
+Patient.sync({alter:true, force:forceUpdate})
 
 
 module.exports = {
