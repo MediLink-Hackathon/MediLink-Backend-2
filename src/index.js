1cc44f7ebd989e03389372df45379bb3448f73c0
AndreNIH <31828626+AndreNIH@users.noreply.github.com>
2023-09-10 00:51:26 -0700

    Fix database connection code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ require('dotenv').config()
 
 const app = require('./app')
 const config =  require('./config/server')
+const {connectToDatabase} = require('./utils/database')
 
-app.listen(config.PORT, () => {
-    console.log(`Server running on port ${config.PORT}`)
-})
+const start = async () => {
+    await connectToDatabase()
+    app.listen(config.PORT, () => {
+        console.log(`Server running on port ${config.PORT}`)
+    })
+}
+
+start()
--- src/utils/database.js
+++ src/utils/database.js
@@ -1,15 +1,35 @@
+const tedious = require('tedious')
 const Sequelize = require('sequelize')
-const {DB_HOST} = require('../config/database').DB_HOST
+const DB_CONFIG = require('../config/database.config')
 
-const sequelize = new Sequelize(DB_HOST)
+
+
+const sequelize = new Sequelize(
+    
+    DB_CONFIG.DATABACE_INSTANCE,
+    DB_CONFIG.DATABASE_USER,
+    DB_CONFIG.DATABASE_PASSWORD,{
+        dialect:'mssql',
+        host: DB_CONFIG.DATABASE_HOST,
+        dialectModule: tedious,
+        dialectOptions:{
+            options: {
+                encrypt: true,
+                
+            }
+        }
+
+    }
+    
+)
+
 
 const connectToDatabase = async () => {
     try{
-        const sequelize = new Sequelize(DATABASE_URL)
         await sequelize.authenticate()
         console.log('Connection has been established successfully.');
     }catch(err){
-        console.log('Failed to connecto to the database')
+        console.log('Failed to connecto to the database:', err)
         return process.exit(1)
     }
     return null
