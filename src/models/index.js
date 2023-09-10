21425eacb41391cf2c023d3b0a2cfab8fdac0332
AndreNIH <31828626+AndreNIH@users.noreply.github.com>
2023-09-10 01:26:26 -0700

    Sync model schemas

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,9 @@
 const Psychologist = require('./psychologist')
 const User = require('./user')
 
+//Create tables matching model schema
+User.sync()
+Psychologist.sync()
+
+
 module.exports = {User, Psychologist}
